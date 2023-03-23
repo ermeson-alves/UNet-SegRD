054206d857f11fdadc99c913e3c7956b644fc24b
Ermeson Alves <ermeson3alves@gmail.com>
2023-03-23 17:59:54 -0300

    First Commit

---
+++ config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Paths necessarios (Para DIARETDB1):
+ROOT_DATASET_PATH = Path("datasets/diaretdb1_v_1_1")
+IMGS_FUNDUS_PATH = Path("datasets/diaretdb1_v_1_1/resources/images/ddb1_fundusimages")
+MASKS_DIR_PATH = Path("datasets/diaretdb1_v_1_1/resources/images/ddb1_groundtruth")
+ANNOTATIONS_TRAIN_PATH = Path("datasets/diaretdb1_v_1_1/resources/traindatasets/trainset.txt")
+ANNOTATIONS_TEST_PATH = Path("datasets/diaretdb1_v_1_1/resources/testdatasets/testset.txt")
